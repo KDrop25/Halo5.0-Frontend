e4611896c4377f66e3eb4a4e8ca58af16d9d6e7d
Karan Agrawal <karan25.pvt@gmail.com>
2023-08-25 18:37:20 +0530

    universal analyics id updated

--- src/hooks/googleanalytics.js
+++ src/hooks/googleanalytics.js
@@ -2,24 +2,21 @@ import React, { useEffect } from 'react';
 
 function GoogleAnalytics() {
   useEffect(() => {
-    // Load the gtag.js library
     const script = document.createElement('script');
-    script.src = 'https://www.googletagmanager.com/gtag/js?id=G-TW3MWFPJ1D';
+    script.src = 'https://www.googletagmanager.com/gtag/js?id=G-F9RZWEMYD0';
     script.async = true;
     document.head.appendChild(script);
 
-    // Initialize the dataLayer and define gtag function
     window.dataLayer = window.dataLayer || [];
     function gtag() {
       window.dataLayer.push(arguments);
     }
     gtag('js', new Date());
 
-    // Configure Google Analytics
-    gtag('config', 'G-TW3MWFPJ1D');
+    gtag('config', 'G-F9RZWEMYD0');
   }, []);
 
-  return null; // This component doesn't render anything
+  return null;
 }
 
 export default GoogleAnalytics;
