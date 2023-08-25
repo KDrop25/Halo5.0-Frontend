3e948983b2d6cc5fb53f557ea3820146e07f44e6
Karan Agrawal <karan25.pvt@gmail.com>
2023-08-25 17:13:21 +0530

    Update Footer.jsx

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -18,8 +18,8 @@ const Menu = () => (<>
   <Link to="/schedule"><p className='navbar-schedule-text'>Schedule</p></Link>
   <Link to="/events"><p className='navbar-events-text'>Events</p></Link>
   <Link to="/gallery"><p className='navbar-contact-text'>Gallery</p></Link>
-  <Link to="/contact"><p className='navbar-contact-text'>Contact</p></Link>
-  
+
+
 </>)
 
 
@@ -29,45 +29,45 @@ const Menu = () => (<>
 
 const Footer = () => {
   return (
-    
-      <div class="footer">
-        <div class="top-panel">
-          <div class="left-panel">
-            <div className='left-panel-content'>
-              <Menu/>
-              <div>
-                <a href="https://www.facebook.com"><img class = "facebook-image" src={facebook} alt="Facebook"/></a>
-                
-                <a href="https://www.instagram.com/halocon.official/"><img class = "instagram-image" src={instagram} alt="Facebook"/></a>
-                
-                <a href="www.facebook.com"><img class = "twitter-image" src={twitter} alt="Facebook"/></a>
-                
-                <a href="https://www.youtube.com/channel/UCA4Hn06L6b99i9bgqmhtN1Q"><img class = "youtube-image" src={youtube} alt="Facebook"/></a>
-                
-                <a href="www.facebook.com"><img class = "twitch-image" src={twitch} alt="Facebook"/></a>
-                
-                <a href="www.facebook.com"><img class = "twitch-image" src={github} alt="Facebook"/></a>
-              </div>
-            </div>
-          </div>
-          <div class="right-panel">
-            <div className='right-panel-content'>
-              <p className='footer-email-text'><img class = "mail-image" src={mail} alt="Email:"/><a href="mailto:halolegionofficial@gmail.com">halolegionofficial@gmail.com</a></p>
-              <p><img class = "mobile-phone-image" src={phone} alt="Email:"/><strong>+91 - </strong>9827023035</p>
-              <p><img class = "office-phone-image" src={phone} alt="Email:"/><strong>0731 - </strong>4982904</p>
-              <br></br>
-              <p className='footer-email-text'><img class = "mail-image" src={location} alt="Location"/><a href="mailto:halolegionofficial@gmail.com">Piplia Kumar, Nepania Road,<br/>Near Water Lily, <br/>Nepania,<br/>Indore,Madhya Pradesh, <strong>452016</strong></a></p>
-              <div class="right-privacy-panel">
-                <p class='right-text-1'><strong><a href="privacy">Privacy</a></strong></p>
-                <p class='right-text-2'><strong><a href="terms">Terms & Conditions</a></strong></p>
-              </div>
+
+    <div class="footer">
+      <div class="top-panel">
+        <div class="left-panel">
+          <div className='left-panel-content'>
+            <Menu />
+            <div>
+              <a href="https://www.facebook.com"><img class="facebook-image" src={facebook} alt="Facebook" /></a>
+
+              <a href="https://www.instagram.com/halocon.official/"><img class="instagram-image" src={instagram} alt="Facebook" /></a>
+
+              <a href="www.facebook.com"><img class="twitter-image" src={twitter} alt="Facebook" /></a>
+
+              <a href="https://www.youtube.com/channel/UCA4Hn06L6b99i9bgqmhtN1Q"><img class="youtube-image" src={youtube} alt="Facebook" /></a>
+
+              <a href="www.facebook.com"><img class="twitch-image" src={twitch} alt="Facebook" /></a>
+
+              <a href="www.facebook.com"><img class="twitch-image" src={github} alt="Facebook" /></a>
             </div>
           </div>
         </div>
-        
+        <div class="right-panel">
+          <div className='right-panel-content'>
+            <p className='footer-email-text'><img class="mail-image" src={mail} alt="Email:" /><a href="mailto:halolegionofficial@gmail.com">halolegionofficial@gmail.com</a></p>
+            <p><img class="mobile-phone-image" src={phone} alt="Email:" /><strong>+91 - </strong>9827023035</p>
+            <p><img class="office-phone-image" src={phone} alt="Email:" /><strong>0731 - </strong>4982904</p>
+            <br></br>
+            <p className='footer-email-text'><img class="mail-image" src={location} alt="Location" /><a href="mailto:halolegionofficial@gmail.com">Piplia Kumar, Nepania Road,<br />Near Water Lily, <br />Nepania,<br />Indore,Madhya Pradesh, <strong>452016</strong></a></p>
+            <div class="right-privacy-panel">
+              <p class='right-text-1'><strong><a href="privacy">Privacy</a></strong></p>
+              <p class='right-text-2'><strong><a href="terms">Terms & Conditions</a></strong></p>
+            </div>
+          </div>
+        </div>
       </div>
-    
-    
+
+    </div>
+
+
   )
 }
 
